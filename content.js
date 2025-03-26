868487af6a3132dbe61f3bf78c8b310d34d40fd6
Julian Pagano <jpagano@rosariobus.com.ar>
2025-03-26 15:49:46 -0300

    remove textbox.dispatchEvent

--- content.js
+++ content.js
@@ -2,7 +2,6 @@ function fillTextbox() {
   let textbox = document.querySelector("#q");
   if (textbox) {
     textbox.value = "-is:ub ";
-    textbox.dispatchEvent(new Event("input", { bubbles: true }));
   } else {
     setTimeout(fillTextbox, 500);
   }
